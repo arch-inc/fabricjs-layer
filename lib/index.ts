b8bf8b98489ea2db1300c7ffa838f4634901c5f3
Jun Kato <i@junkato.jp>
2020-10-15 14:23:04 +0900

    Remove psbrush and add eraser impl

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,4 @@
-import PSBrush, { PSBrushIface } from "./PSBrush";
-import PSPoint from "./PSPoint";
-import PSStroke, { PSStrokeIface } from "./PSStroke";
-import Simplify from "./Simplify";
+import EraserBrush, { EraserBrushIface } from "./EraserBrush";
+import ErasedGroup, { ErasedGroupIface } from "./ErasedGroup";
 
-export { PSBrush, PSBrushIface, PSStroke, PSStrokeIface, PSPoint, Simplify };
+export { EraserBrush, EraserBrushIface, ErasedGroup, ErasedGroupIface };
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,6 @@
+import { fabric } from "fabric";
+import { ErasedGroupIface } from "./ErasedGroup";
+
 export type FabricPointerEvent = TouchEvent | MouseEvent | PointerEvent;
 
 export interface FabricEvent {
@@ -21,4 +24,79 @@ export function getPressure(ev: FabricPointerEvent) {
   }
   // PointerEvent (ev.pointerType: "pen" | "touch")
   return (<PointerEvent>ev).pressure;
+}
+
+export function canvasIsFabric(
+  object: fabric.Canvas | HTMLCanvasElement
+): object is fabric.Canvas {
+  return typeof object["_objects"] !== "undefined";
+}
+
+export function isErasedGroup(
+  object: fabric.Object | fabric.ICollection<any>
+): object is ErasedGroupIface {
+  return object["type"] === "ErasedGroup";
+}
+
+export function isFabricCollection(
+  object: fabric.Object | fabric.ICollection<any>
+): object is fabric.ICollection<any> {
+  if ("getObjects" in object) {
+    return true;
+  }
+
+  return false;
+}
+
+export function getOverlappedObjects(
+  scope: fabric.Group | fabric.Canvas,
+  path: fabric.Path
+) {
+  // get list of objects in the parent scope
+  const allObjects = scope.getObjects();
+
+  // grab all the objects that intersects with the path
+  // and get group-relative position of top-left corner of the filtered objects
+  let left = Number.MAX_VALUE,
+    top = Number.MAX_VALUE,
+    minIntersectionIndex = -1,
+    maxIntersectionIndex = -1;
+
+  allObjects.forEach((obj, i) => {
+    // if (obj instanceof fabric.Textbox) return false;
+    // if (obj instanceof fabric.IText) return false;
+    left = Math.min(left, obj.get("left"));
+    top = Math.min(top, obj.get("top"));
+    const intersection = obj.intersectsWithObject(path);
+    if (intersection) {
+      if (minIntersectionIndex < 0) {
+        minIntersectionIndex = i;
+      }
+      maxIntersectionIndex = i;
+    }
+    return intersection;
+  });
+
+  const objects =
+    minIntersectionIndex < 0
+      ? []
+      : allObjects.slice(minIntersectionIndex, maxIntersectionIndex + 1);
+  return {
+    overlapped: {
+      objects,
+      topLeft: getTopLeft(objects),
+      index: minIntersectionIndex,
+    },
+    all: { objects: allObjects, topLeft: { left, top } },
+  };
+}
+
+export function getTopLeft(objects: fabric.Object[]) {
+  let left = Number.MAX_VALUE,
+    top = Number.MAX_VALUE;
+  objects.forEach((obj) => {
+    left = Math.min(left, obj.get("left"));
+    top = Math.min(top, obj.get("top"));
+  });
+  return { left, top };
 }
