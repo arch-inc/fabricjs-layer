f5bfcdcb1f75464022742c1b2f38920225254735
Jun Kato <i@junkato.jp>
2020-10-15 15:03:10 +0900

    Copy library file as lib.js

--- docs/public/index.js
+++ docs/public/index.js
@@ -6,9 +6,16 @@ function initialize(el) {
     enablePointerEvents: true
   });
 
+  console.log("psbrush", psbrush);
+  console.log("eraser", eraser);
+  
   // Initialize a brush
-  let brush = new fabric.PSBrush(canvas);
+  // let brush = new fabric.PSBrush(canvas);
+  // brush.width = 10;
+  // brush.color = "#000";
+  // canvas.freeDrawingBrush = brush;
+  let brush = new fabric.EraserBrush(canvas, { Simplify: psbrush.Simplify });
   brush.width = 10;
-  brush.color = "#000";
   canvas.freeDrawingBrush = brush;
+
 }
