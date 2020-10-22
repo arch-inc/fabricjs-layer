03f9373dc282f805ebb3d81256ac64c1252f688d
Jun Kato <i@junkato.jp>
2020-10-23 01:13:08 +0900

    Remove unused utility functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,51 +1,10 @@
 import { fabric } from "fabric";
 import { ErasedGroupIface } from "./ErasedGroup";
 
-export type FabricPointerEvent = TouchEvent | MouseEvent | PointerEvent;
-
-export interface FabricEvent {
-  e: FabricPointerEvent;
-  pointer: FabricPointer;
-}
-
-export interface FabricPointer {
-  x: number;
-  y: number;
-}
-
-export function getPressure(ev: FabricPointerEvent) {
-  // TouchEvent
-  if (ev["touches"] && ev["touches"].length > 0) {
-    return (<TouchEvent>ev).touches[0].force;
-  }
-  // MouseEvent, PointerEvent (ev.pointerType: "mouse")
-  if (ev["pointerType"] === "mouse" || typeof ev["pressure"] !== "number") {
-    return 0.5;
-  }
-  // PointerEvent (ev.pointerType: "pen" | "touch")
-  return (<PointerEvent>ev).pressure;
-}
-
-export function canvasIsFabric(
-  object: fabric.Canvas | HTMLCanvasElement
-): object is fabric.Canvas {
-  return typeof object["_objects"] !== "undefined";
-}
-
 export function isErasedGroup(
   object: fabric.Object | fabric.ICollection<any>
 ): object is ErasedGroupIface {
   return object["type"] === "ErasedGroup";
-}
-
-export function isFabricCollection(
-  object: fabric.Object | fabric.ICollection<any>
-): object is fabric.ICollection<any> {
-  if ("getObjects" in object) {
-    return true;
-  }
-
-  return false;
 }
 
 export function getOverlappedObjects(
