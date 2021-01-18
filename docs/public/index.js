d81fabf6ec1e133fd005eab49afce5485e98d91e
Jun Kato <i@junkato.jp>
2021-01-18 09:44:09 +0900

    Test initializing LayerManager

--- docs/public/index.js
+++ docs/public/index.js
@@ -1,3 +1,18 @@
 
-function initialize(_el) {
+function initialize(el) {
+  // Create a Fabric.js canvas
+  let canvas = new fabric.Canvas(el, {
+    isDrawingMode: true,
+    enablePointerEvents: true
+  });
+
+  // Initialize a brush
+  let brush = new fabric.PSBrush(canvas);
+  brush.width = 10;
+  brush.color = "#000";
+  canvas.freeDrawingBrush = brush;
+
+  // Initialize a layer manager
+  const manager = new layer.LayerManager(canvas);
+  console.log("manager", manager);
 }
--- lib/LayerManager.ts
+++ lib/LayerManager.ts
@@ -4,7 +4,7 @@
  * fabricjs-layer, lightweight layer management for Fabric.js
  * @license MIT
  */
-import { LayerIface } from "./Layer";
+import Layer, { LayerIface } from "./Layer";
 
 const fabricjs: typeof fabric =
   typeof fabric === "undefined" ? require("fabric").fabric : fabric;
@@ -12,11 +12,14 @@ const fabricjs: typeof fabric =
 class LayerManager {
   activeLayer: LayerIface;
   constructor(private canvas: fabric.StaticCanvas) {
+    this.activeLayer = new Layer();
     canvas.on("object:added", (e) => {
       // monitor object addition
+      console.log("added", e);
     });
     canvas.on("object:removed", (e) => {
       // monitor object deletion
+      console.log("removed", e);
     });
   }
 }
