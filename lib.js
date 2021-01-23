c6088e2e6fc018406994120bdac19e793aea00a4
arcatdmz <arcatdmz@users.noreply.github.com>
2021-01-23 08:16:25 +0000

    deploy: 272c28103baa4e75a5c28a4c86ccecfd81409b63

--- lib.js
+++ lib.js
@@ -1,7 +1,7 @@
-!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?t(exports):"function"==typeof define&&define.amd?define(["exports"],t):t((e="undefined"!=typeof globalThis?globalThis:e||self).layer={})}(this,(function(e){"use strict";
+!function(e,o){"object"==typeof exports&&"undefined"!=typeof module?o(exports):"function"==typeof define&&define.amd?define(["exports"],o):o((e="undefined"!=typeof globalThis?globalThis:e||self).layer={})}(this,(function(e){"use strict";
 /*!
    * Copyright (c) 2020 Arch Inc. (Jun Kato)
    *
-   * fabricjs-layer, a lightweight layer implementation for Fabric.js
+   * fabricjs-layer, lightweight layer management for Fabric.js
    * @license MIT
-   */var t="undefined"==typeof fabric?require("fabric").fabric:fabric,i=t.util.createClass(t.Group,{type:"LayerGroup",createdTime:null,initialize:function(e,t,i){t=t||{},this.callSuper("initialize",e,t,i),this.createdTime=t.createdTime},toObject:function(e){return this.callSuper("toObject",["createdTime"].concat(e))}});i.fromObject=function(e,i){t.util.enlivenObjects(e.objects,(function(r){t.util.enlivenObjects([e.clipPath],(function(o){var c=t.util.object.clone(e,!0);c.clipPath=o[0],delete c.objects,i&&i(new n(r,c,!0))}),null,null)}),null,null)};var n=i;t.LayerGroup=n,e.LayerGroup=n,e.isLayerGroup=function(e){return e&&"LayerGroup"===e.type},Object.defineProperty(e,"__esModule",{value:!0})}));
+   */"undefined"==typeof fabric?require("fabric").fabric:fabric;var o=function(){this.type="Layer",this.createdTime=Date.now()},i=("undefined"==typeof fabric?require("fabric").fabric:fabric,function(e){this.canvas=e,this.activeLayer=new o,e.on("object:added",(function(e){console.log("added",e)})),e.on("object:removed",(function(e){console.log("removed",e)}))});e.LayerGroup=o,e.LayerManager=i,e.isLayerGroup=function(e){return e&&"Layer"===e.type},Object.defineProperty(e,"__esModule",{value:!0})}));
