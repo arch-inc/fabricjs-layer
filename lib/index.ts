138f3a46e8a3945443ef80212dd4324e8daa6be6
Jun Kato <i@junkato.jp>
2021-10-17 00:09:24 +0900

    Export missing interface

--- lib/index.ts
+++ lib/index.ts
@@ -1,8 +1,8 @@
 import LayerGroup, { LayerIface } from "./Layer";
-import LayerManager from "./LayerManager";
+import LayerManager, { LayerManagerIface } from "./LayerManager";
 export * from "./LayerEvent";
 export * from "./LayerEventListener";
 export * from "./LayerManagerEvent";
 export * from "./LayerManagerEventListener";
 export * from "./utils";
-export { LayerGroup, LayerIface, LayerManager };
+export { LayerGroup, LayerIface, LayerManager, LayerManagerIface };
