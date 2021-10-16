70c3b7a3e7aa656eb433bca863ceacd0b098c817
arcatdmz <arcatdmz@users.noreply.github.com>
2021-10-16 15:11:16 +0000

    deploy: 138f3a46e8a3945443ef80212dd4324e8daa6be6

---
+++ _next/static/MescQKbllMOsrYHt0h-jk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/781-2c454609a824dfe90b87.js","static/chunks/pages/index-e3d1f8dbcab8b73c5f8c.js"],"/_error":["static/chunks/pages/_error-e5a81b6e5d94da8014de.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
